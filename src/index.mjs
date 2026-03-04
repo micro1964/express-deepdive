19b40412f50ac5c3828c72a8aca2da5af20320ac
reggy williams <reggywilliams@gmail.com>
2026-03-04 02:11:49 +0000

    Added new endpoint that returns a specified user

--- src/index.mjs
+++ src/index.mjs
@@ -5,17 +5,31 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const mockUsers = [
+        { id: 1, name: "Alice" },
+        { id: 2, name: "Bob" },
+        { id: 3, name: "Charlie" }
+    ];
+
 app.get('/', (request, response) => {
   response.status(200).json({ message: "Hey! This is a test message." });
 });
 
 app.get('/api/users', (request, response) => {
-    const users = [
-        { id: 1, name: "Alice" },
-        { id: 2, name: "Bob" },
-        { id: 3, name: "Charlie" }
-    ];
-    response.status(200).json(users);
+    response.status(200).json(mockUsers);
+});
+
+app.get("/api/users/:id", (request, response) => {
+    const parsedUser = parseInt(request.params.id);
+    console
+    if (isNaN(parsedUser)) {
+        return response.status(400).json({ error: "Invalid user ID" });
+    }
+    const findUser = mockUsers.find(user => user.id === parsedUser);
+    if (!findUser) {
+        return response.status(404).json({ error: "User not found" });
+    }
+    response.status(200).json(findUser);
 });
 
 app.get('/api/products', (request, response) => {
